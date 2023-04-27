d44a94db9b129c008966189a6c00bf026f2f8fe9
Leandro <leandro.david.dini@gmail.com>
2023-04-27 01:03:58 -0300

    Entregable

--- Natom.ATSA.Colegio/App_Start/RouteConfig.cs
+++ Natom.ATSA.Colegio/App_Start/RouteConfig.cs
@@ -15,7 +15,8 @@ namespace Natom.ATSA.Colegio
 
             routes.MapRoute(
                 name: "Default",
-                url: "colegiotest/{controller}/{action}/{id}",
+                url:"{controller}/{action}/{id}",
+                //url: "colegiotest/{controller}/{action}/{id}",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
             );
         }
